d365aa33e4349336181f33245b62b6df198671c0
Farhan Sadik Galib <farhansadikgalib@gmail.com>
2023-01-03 12:49:20 +0600

    Initial Commit

---
+++ card-challange/card_challangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  card_challangeApp.swift
+//  card-challange
+//
+//  Created by Farhan Sadik on 3/1/23.
+//
+
+import SwiftUI
+
+@main
+struct card_challangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
